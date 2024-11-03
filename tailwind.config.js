a4ec7dc8b7a0385a82d315d8c5616a5cfa5f98aa
chegudev <chegudeveloper@gmail.com>
2024-11-03 15:49:01 +0330

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        ANregular: ['AvenirNext-Regular'],
+        ANbold: ['AvenirNext-DemiBold'],
+        ANmedium: ['AvenirNext-Medium'],
+      },
+    },
+  },
+  plugins: [],
+}
